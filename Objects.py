0402acff6f84916a3560af0cef0eccc343de3b67
riffuu1 <pj85gyl@eduvaud.ch>
2026-03-10 10:39:56 +0100

    refactor:PickableObjects

--- Objects.py
+++ Objects.py
@@ -1,4 +1,5 @@
 import pygame
+from Item import Item
 
 class Object():
     def __init__(self, image, x, y):
@@ -13,14 +14,14 @@ class Object():
     def interact(self):
         pass
 
-class Pickableobject(Object):
-    def __init__(self, x, y, image, item_name):
+class PickableObject(Object):
+    def __init__(self, x, y, image, item):
         # Le super sert a prendre les paramètre de la classe parents (self.rect = image.get_rect(topleft=(x, y)), self.image = image, self.active = True
-        super().__init__(self,x,y,image)
-        self.item_name = item_name
+        super().__init__(x,y,image)
+        self.item = item
 
     def interact(self,player,keys):
         if self.rect.colliderect(player.feet):
             if keys[pygame.K_e]:
-                player.add_item(self.item_name)
+                player.add_item(self.item)
                 self.active = False
